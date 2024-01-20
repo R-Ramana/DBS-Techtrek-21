dec44a79b86224e88332af37931bfe910acde05e
Jiang Nuoyi <jiangnuoyi1999@gmail.com>
2024-01-20 11:59:41 +0800

    merge itinery branch

--- dbs-21/src/Dashboard/CreateIt.jsx
+++ dbs-21/src/Dashboard/CreateIt.jsx
@@ -1,5 +1,6 @@
 import React, {useState} from 'react';
-import {countries, destinations} from './testData.js'
+import {countries, destinations} from './testData.js';
+import './CreateIt.css';
 
 const CreateIt=()=>{
     const [itData, setItData] = useState({
@@ -9,8 +10,8 @@ const CreateIt=()=>{
       });
     
     const [destination, setDestinations]= useState([]);
-    
-      const handleChange = (e) => {
+
+    const handleChange = (e) => {
         const { name, value } = e.target;
         const parsedValue = name === 'budget' ? parseFloat(value) : value;
         setItData({ ...itData, [name]: parsedValue });
@@ -54,31 +55,28 @@ const CreateIt=()=>{
       };
     
       return (
-        <form onSubmit={handleSubmit}>
-          <label>
+        <form className = 'itinery_form' onSubmit={handleSubmit}>
+          <label className='itinery_line'>
             Title:
             <input type="text" name="title" value={itData.title} onChange={handleChange} />
           </label>
-          <br />
-          <label>
+          <label className='itinery_line'>
             Budget:
             <input type = 'number' name="budget" value={itData.budget} onChange={handleChange} />
           </label>
-          <br />
-          <select name='country_id' value={itData.country_id} onChange={handleChange}>
+          <select className='itinery_dropdown' name='country_id' value={itData.country_id} onChange={handleChange}>
             {countries.map((c)=>(
                 <option key={c.country_id} value={c.country_id}>{c.name}</option>
             ))}
           </select>
-          <br />
-          <select multiple name='destination' value={destination} onChange={handleDestionation}>
+
+          <select className='itinery_dropdown' multiple name='destination' value={destination} onChange={handleDestionation}>
             {destinations.map((d) => (
                 <option key={d.id} value={d.id}>
                 {d.name}
              </option>
              ))}
           </select>
-          <br/>
           <button type="submit">Submit</button>
         </form>
       );
--- dbs-21/src/Dashboard/testData.js
+++ dbs-21/src/Dashboard/testData.js
@@ -5,5 +5,6 @@ export const countries =[{
 
 export const destinations = [{
     id:1, country_id:1, cost:50, name:"marina bay", notes:'hotel'
-}, {id:2, country_id:1, cost:30, name:"gardens by the bay", notes:'garden'}
+}, {id:2, country_id:1, cost:30, name:"gardens by the bay", notes:'garden'},
+{id:3, country_id:2, cost:100, name:'Great Wall', notes:'must visit'}
 ]
