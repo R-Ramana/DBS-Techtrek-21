bbb1fd4ab28f6ed4286777737a31758d537e7905
iamhannah <hereismytrashbin@gmail.com>
2024-01-20 12:46:03 +0800

    update itinerary.jsx

--- dbs-21/src/components/Itinerary.jsx
+++ dbs-21/src/components/Itinerary.jsx
@@ -2,11 +2,15 @@ import React from 'react'
 import Destination from './Destination'
 
 const Itinerary = ({itinerary, onDelete, onEdit}) => {
+  const h3Style = {
+    textAlign: 'left',
+  };
   return (
+   
     <div className= 'itinerary'>
     <h1>Title: {itinerary.title} </h1>
     <h2>Budget: ${itinerary.budget}</h2>
-     <h3> Destinations </h3>
+     <h3 style={h3Style}> Destinations </h3>
     {itinerary.destinations.map((destination, index) => (
         <Destination key={index} destination={destination} onDelete={onDelete} onEdit={onEdit} />
        ))} 
