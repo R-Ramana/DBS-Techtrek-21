f09312fbef0e917496a0c75793cca9581eec7bb1
Jiang Nuoyi <jiangnuoyi1999@gmail.com>
2024-01-20 12:59:33 +0800

    removed unused dashboard

--- dbs-21/src/App.js
+++ dbs-21/src/App.js
@@ -2,7 +2,6 @@ import React from 'react';
 import {Routes, Route} from 'react-router-dom';
 import './App.css';
 import NavBar from './Component/Navbar.jsx';
-import Dashboard from './Dashboard/Dashboard.jsx';
 import CreateIt from './Dashboard/CreateIt.jsx';
 import Login from "./pages/Login";
 import Register from "./pages/Register";
@@ -11,35 +10,6 @@ import Itinerary from './components/Itinerary'
 
 import {useState, useEffect} from 'react'
 function App() {
-  const [itinerary,setItineraries] = useState({
-    id:1,
-    budget: 500,
-    title: "Singapore Trip",
-    destinations: [{
-      id: 1,
-      country_id: 1,
-      cost:50,
-      name: 'Marina Bay Sands',
-      notes: 'Iconic hotel with an infinity pool and stunning views of the city skyline. Open 24/7.'
-    },
-    {
-      id: 2,
-      country_id: 1,
-      cost:500,
-      name: 'Gardens by the Bay',
-      notes: 'Futuristic park featuring Supertree Grove and Flower Dome conservatories. Open daily from 9 AM to 9 PM.'
-    },
-    {
-      id: 3,
-      country_id: 1,
-      cost:50,
-      name: 'Sentosa Island',
-      notes: 'Fun-filled island resort with beaches, theme parks, and various attractions. Open daily from 10 AM to 7 PM.'
-    }
-  ]
-}
-    )
-
     const deleteItinerary = (id) => {
       console.log(id)
     }
