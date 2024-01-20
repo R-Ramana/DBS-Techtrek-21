dcdc82b5b4e40ae1a63121991ed1b3fc31e9d411
Ramana <ramanalfc@yahoo.com.sg>
2024-01-20 12:35:35 +0800

    server: register user

--- server/routing/Users.js
+++ server/routing/Users.js
@@ -2,30 +2,59 @@ const express = require("express");
 const router = express.Router();
 const { User } = require("../models");
 
-router.get("/", async (req, res) => {
-  const listOfPosts = await User.findAll();
-  res.json(listOfPosts);
+router.get("/", async(req, res) => {
+    const listOfPosts = await User.findAll();
+    res.json(listOfPosts);
 });
 
-router.get("/byId/:id", async (req, res) => {
-  //":parameterName" value is inside req.params.parameterName
-  const id = req.params.id;
-  const post = await User.findByPk(id);
-  res.json(post);
-});
-
-router.post("/123", async (req, res) => {
-  try {
-    //receive a FORM from frontend in JSON
-    const post = req.body;
-    //this body data should have the same format as our
-    //Post.js format in
-    await User.create(post);
-    //sequelize is called here to create and insert that object into db
+router.get("/byId/:id", async(req, res) => {
+    //":parameterName" value is inside req.params.parameterName
+    const id = req.params.id;
+    const post = await User.findByPk(id);
     res.json(post);
-  } catch (error) {
-    res.send(error);
-  }
+});
+
+router.post("/register", async(req, res) => {
+    const firstname = req.body.first_name;
+    const lastname = req.body.last_name;
+    const username = req.body.username;
+    const password = req.body.password;
+
+    try {
+        // Check if the user already exists
+        const existingUser = await User.findOne();
+
+        if (existingUser) {
+            return res.status(400).json({
+                error: "Username already exists, choose a new name or sign in.",
+            });
+        }
+
+        // Hash the password using crypto & salt
+        const salt = crypto.randomBytes(128).toString("base64");
+        const hash_password = crypto.pbkdf2Sync(
+            password,
+            salt,
+            10000,
+            512,
+            "sha512"
+        );
+
+        // Create a new user in the database with all info including salt
+        User.create({
+            first_name: firstname,
+            last_name: lastname,
+            username: username,
+            password: hash_password,
+            salt: salt,
+        });
+
+        return res.status(201).json({ message: "User registered successfully" });
+    } catch (err) {
+        return res
+            .status(500)
+            .json({ error: "We encountered an error, please try again" });
+    }
 });
 
 module.exports = router;
--- server/server.js
+++ server/server.js
@@ -4,7 +4,7 @@ const crypto = require("crypto");
 
 const express = require("express");
 const app = express();
-
+app.use(express.json());
 const db = require("./models");
 const { User } = require("./models/User");
 
@@ -15,82 +15,38 @@ const PORT = 3001;
 
 // respond with "hello world" when a GET request is made to the homepage - test GET
 app.get("/", (req, res) => {
-  res.send("hello world");
+    res.send("hello world");
 });
 
-// Route for user registration
-app.post("/register", async (req, res) => {
-  const firstname = req.body.fname;
-  const lastname = req.body.lname;
-  const username = req.body.username;
-  const password = req.body.password;
+app.post("/login", async(req, res) => {
+    // TO auth user
+    const username = req.body.username;
+    const password = req.body.password;
 
-  try {
-    // Check if the user already exists
-    const existingUser = await User.findOne();
+    try {
+        // Check if the user exists in the database
 
-    if (existingUser) {
-      return res.status(400).json({
-        error: "Username already exists, choose a new name or sign in.",
-      });
+        if (!user) {
+            return res.status(401).json({ error: "Invalid credentials" });
+        }
+
+        // Compare the provided password with the hashed password
+        // get salt from db, then compare with hashed pw
+        const encryptHash = crypto.pbkdf2Sync(password, salt, 10000, 512, "sha512");
+        // compare encryptHash with pw
+
+        if (!passwordMatch) {
+            return res.status(401).json({ error: "Invalid credentials" });
+        }
+
+        // Generate a JWT for the authenticated user
+        // get token and send to db
+        // const access_token = jwt.sign(process.env.ACCESS_SECRET_TOKEN)
+        // jwt.sign({ username: username, password: password }, access_token)
+        // return res.status(200).json({ token });
+    } catch (err) {
+        return res.status(500).json({ error: "Internal server error" });
     }
-
-    // Hash the password using crypto & salt
-    const salt = crypto.randomBytes(128).toString("base64");
-    const hash_password = crypto.pbkdf2Sync(
-      password,
-      salt,
-      10000,
-      512,
-      "sha512"
-    );
-
-    // Create a new user in the database with all info including salt
-    User.create({
-      first_name: firstname,
-      last_name: lastname,
-      username: username,
-      password: hash_password,
-      salt: salt,
-    });
-
-    return res.status(201).json({ message: "User registered successfully" });
-  } catch (err) {
-    return res
-      .status(500)
-      .json({ error: "We encountered an error, please try again" });
-  }
-});
-
-app.post("/login", async (req, res) => {
-  // TO auth user
-  const username = req.body.username;
-  const password = req.body.password;
-
-  try {
-    // Check if the user exists in the database
-
-    if (!user) {
-      return res.status(401).json({ error: "Invalid credentials" });
-    }
-
-    // Compare the provided password with the hashed password
-    // get salt from db, then compare with hashed pw
-    const encryptHash = crypto.pbkdf2Sync(password, salt, 10000, 512, "sha512");
-    // compare encryptHash with pw
-
-    if (!passwordMatch) {
-      return res.status(401).json({ error: "Invalid credentials" });
-    }
-
-    // Generate a JWT for the authenticated user
-    // get token and send to db
-    // const access_token = jwt.sign(process.env.ACCESS_SECRET_TOKEN)
-    // jwt.sign({ username: username, password: password }, access_token)
-    // return res.status(200).json({ token });
-  } catch (err) {
-    return res.status(500).json({ error: "Internal server error" });
-  }
 });
 
 // app.get('/user/data', checkToken, (req, res) => {
@@ -116,7 +72,7 @@ app.post("/login", async (req, res) => {
 // });
 
 db.sequelize.sync().then(() => {
-  app.listen(PORT, () => {
-    console.log(`Example app listening on port ${PORT}`);
-  });
+    app.listen(PORT, () => {
+        console.log(`Example app listening on port ${PORT}`);
+    });
 });
