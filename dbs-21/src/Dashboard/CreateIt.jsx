7da857db3670539544dedf3bb50974dc3d682d22
Jiang Nuoyi <jiangnuoyi1999@gmail.com>
2024-01-20 11:35:31 +0800

    added sample data for select

--- dbs-21/src/Dashboard/CreateIt.jsx
+++ dbs-21/src/Dashboard/CreateIt.jsx
@@ -1,17 +1,31 @@
 import React, {useState} from 'react';
+import {countries, destinations} from './testData.js'
 
 const CreateIt=()=>{
     const [itData, setItData] = useState({
         'country_id':'',
         'budget':'',
-        'title':''
+        'title':'',
       });
+    
+    const [destination, setDestinations]= useState([]);
     
       const handleChange = (e) => {
         const { name, value } = e.target;
         const parsedValue = name === 'budget' ? parseFloat(value) : value;
         setItData({ ...itData, [name]: parsedValue });
       };
+
+      const handleDestionation = (e)=>{
+        const { value } = e.target;
+        setDestinations((prevSelected) => {
+            if (prevSelected.includes(value)) {
+                return prevSelected.filter((item) => item !== value);
+            } else {
+             return [...prevSelected, value];
+            }
+            });
+      }
     
       const handleSubmit = async (e) => {
         e.preventDefault();
@@ -52,10 +66,19 @@ const CreateIt=()=>{
           </label>
           <br />
           <select name='country_id' value={itData.country_id} onChange={handleChange}>
-            <option name='country_id' value='1'>Country 1</option>
-            <option name='country_id' value='2'>Country 2</option>
+            {countries.map((c)=>(
+                <option key={c.country_id} value={c.country_id}>{c.name}</option>
+            ))}
           </select>
-          
+          <br />
+          <select multiple name='destination' value={destination} onChange={handleDestionation}>
+            {destinations.map((d) => (
+                <option key={d.id} value={d.id}>
+                {d.name}
+             </option>
+             ))}
+          </select>
+          <br/>
           <button type="submit">Submit</button>
         </form>
       );
